d3ef0adb6d206ac3328626fd5a426836edf11321
Zzrk <1252124617@qq.com>
2023-01-20 23:57:40 +0800

    feat: ChromeTabs props

--- src/lib/ChromeTabs/index.tsx
+++ src/lib/ChromeTabs/index.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 
 export interface TabInfo {
   key: string,
-  icon?: string,
+  icon?: string | false,
   title?: string,
   [key: string]: any,
 }
@@ -28,11 +28,19 @@ const ChromeTabs = (
 
     el?.addEventListener('activeTabChange', ({ detail }) => {
       const key = detail.tabEl.getAttribute('data-key');
-      setTabs(tabs.map(tab => tab.key === key ? { ...tab, active: "true" } : { ...tab, active: 'false' }))
+      setTabs(tabs.map(tab => tab.key === key ? { ...tab, active: 'true' } : { ...tab, active: 'false' }))
       onActiveTabChange(tabs, key)
     })
-    el?.addEventListener('tabAdd', ({ detail }) => onTabAdd(detail.tabEl.getAttribute('data-key')))
-    el?.addEventListener('tabRemove', ({ detail }) => onTabRemove(detail.tabEl.getAttribute('data-key')))
+    el?.addEventListener('tabAdd', ({ detail }) => {
+      const key = detail.tabEl.getAttribute('data-key');
+      setTabs([...tabs.map(tab => ({ ...tab, active: 'false' })), { key: String(tabs.length), icon: false, title: 'New tab' }]);
+      onTabAdd(tabs, key);
+    })
+    el?.addEventListener('tabRemove', ({ detail }) => {
+      const key = detail.tabEl.getAttribute('data-key');
+      setTabs(tabs.filter(tab => tab.key !== key));
+      onTabRemove(tabs, key);
+    })
   }, [])
 
   return (
@@ -46,7 +54,7 @@ const ChromeTabs = (
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><symbol id="chrome-tab-geometry-left" viewBox="0 0 214 36"><path d="M17 0h197v36H0v-2c4.5 0 9-3.5 9-8V8c0-4.5 3.5-8 8-8z" /></symbol><symbol id="chrome-tab-geometry-right" viewBox="0 0 214 36"><use xlinkHref="#chrome-tab-geometry-left" /></symbol><clipPath id="crop"><rect className="mask" width="100%" height="100%" x="0" /></clipPath></defs><svg width="52%" height="100%"><use xlinkHref="#chrome-tab-geometry-left" width="214" height="36" className="chrome-tab-geometry" /></svg><g transform="scale(-1, 1)"><svg width="52%" height="100%" x="-100%" y="0"><use xlinkHref="#chrome-tab-geometry-right" width="214" height="36" className="chrome-tab-geometry" /></svg></g></svg>
               </div>
               <div className="chrome-tab-content">
-                <div className="chrome-tab-favicon" style={{ backgroundImage: `url(${tab.icon})` }}></div>
+                <div className="chrome-tab-favicon" style={tab.icon ? { backgroundImage: `url(${tab.icon})` } : {}}></div>
                 <div className="chrome-tab-title">{tab.title}</div>
                 <div className="chrome-tab-drag-handle"></div>
                 <div className="chrome-tab-close"></div>
